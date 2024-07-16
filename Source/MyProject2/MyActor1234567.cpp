5e837d4d808b88d75fa2d04f38a5a3041daadce5
m1kky <rysla@DESKTOP-RKLCQQR>
2024-07-16 23:14:14 +0300

    вффцввф

---
+++ Source/MyProject2/MyActor1234567.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor1234567.h"
+
+// Sets default values
+AMyActor1234567::AMyActor1234567()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyActor1234567::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyActor1234567::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
